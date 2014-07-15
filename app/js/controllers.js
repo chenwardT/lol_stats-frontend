618238779f633250853d169c80eacc77fc6589b6
Edward Chen <kreychek@gmail.com>
2014-07-15 07:08:18 -0400

    Minor changes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,100 +30,96 @@ lolApp.controller('SummonerListCtrl', ['$scope', 'summonerService', function($sc
   $scope.orderProp = 'name';
 }]);
 
-lolApp.controller('SummonerDetailCtrl', ['$scope', '$routeParams', '$http', 'summonerService',
-  function($scope, $routeParams, $http, summonerService) {
-    $scope.summoner = summonerService.get({region: $routeParams.region, name: $routeParams.name});
-    $scope.loading = false;
+lolApp.controller('SummonerDetailCtrl',
+  ['$scope', '$routeParams', '$http', 'summonerService', 'matchHistoryService',
+    function($scope, $routeParams, $http, summonerService, matchHistoryService) {
+      $scope.summoner = summonerService.get({region: $routeParams.region, name: $routeParams.name});
+      $scope.loading = false;   // This lets us toggle things in the view. (i.e. ng-show)
 
-    // TODO: This should probably be in a service.
-    $scope.updateSummonerFromURL = function() {
-      $scope.loading = true;
-      var responsePromise = $http({
-        method: 'POST',
-        url: 'http://127.0.0.1:8001/summoner/ajax_query_start',
-        data: {region: $routeParams.region, name: $routeParams.name},
-        headers: {'content-type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        }
-      });
+      // TODO: The $http call should probably be in a service.
+      $scope.updateSummonerFromURL = function() {
+        $scope.loading = true;
+        var responsePromise = $http({
+          method: 'POST',
+          url: 'http://127.0.0.1:8001/summoner/ajax_query_start',
+          data: {region: $routeParams.region, name: $routeParams.name},
+          headers: {'content-type': 'application/x-www-form-urlencoded'},
+          // This transforms the JSON data into a format that Django natively expects.
+          transformRequest: function(obj) {
+            var str = [];
+            for(var p in obj)
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+          }
+        });
 
-      responsePromise.success(function(data, status) {
-        console.log('success on updateSummonerFromURL...');
-        console.log('data: ' + data);
-        console.log('status: ' + status);
+        // When we request a refresh, we get a taskID back.
+        // We then periodically poll for this task's state until we get "SUCCESS".
+        responsePromise.success(function(data, status) {
+          console.log('success on updateSummonerFromURL...');
+          console.log('data: ' + data);
+          console.log('status: ' + status);
 
-        // This is the task ID we query repeatedly to find out when the update is done.
-        $scope.taskID = data;
+          // This is the task ID we query repeatedly to find out when the update is done.
+          $scope.taskID = data;
 
-        if (timerID) {
-          clearInterval(timerID);
-        }
+          // In case of browser weirdness.
+          if (timerID) {
+            clearInterval(timerID);
+          }
 
-        var timerID = setInterval(function() {
-          var taskResponsePromise = $http({
-            method: 'POST',
-            url: 'http://127.0.0.1:8001/api/task_state',
-            data: {task_id: $scope.taskID},
-            headers: {'content-type': 'application/x-www-form-urlencoded'},
-            transformRequest: function (obj) {
-              var str = [];
-              for (var p in obj)
-                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-              return str.join("&");
-            }
-          });
+          // Here we setup the periodic checking of task state.
+          var timerID = setInterval(function() {
+            var taskResponsePromise = $http({
+              method: 'POST',
+              url: 'http://127.0.0.1:8001/api/task_state',
+              data: {task_id: $scope.taskID},
+              headers: {'content-type': 'application/x-www-form-urlencoded'},
+              transformRequest: function (obj) {
+                var str = [];
+                for (var p in obj)
+                  str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+              }
+            });
 
-          taskResponsePromise.success(function(data, status) {
-            console.log('success on taskResponsePromise...');
-            // Django returns SUCCESS in double quotes here, so check for them...
-            if(data=='"SUCCESS"') {
+            // Each time we get a valid response on the task state.
+            // We should only ever see PENDING or SUCCESS.
+            taskResponsePromise.success(function(data, status) {
+              console.log('success on taskResponsePromise...');
+              // Django returns the string in double quotes specifically.
+              if(data=='"SUCCESS"') {
+                console.log(data);
+                console.log(status);
+                clearInterval(timerID);
+                $scope.loading = false;   // Update our model so the view can change.
+              }
+            });
+
+            taskResponsePromise.error(function(data, status) {
+              console.log('error on taskResponsePromise...');
               console.log(data);
               console.log(status);
-              clearInterval(timerID);
-              $scope.loading = false;
-            }
-          });
+            });
+          }, 300);    // We run the above every 300 ms.
 
-          taskResponsePromise.error(function(data, status) {
-            console.log('error on taskResponsePromise...');
-            console.log(data);
-            console.log(status);
-          });
-        }, 300);
+        });
 
-      });
-      responsePromise.error(function(data, status) {
-        console.log('error on updateSummonerFromURL...');
-        console.log('data: ' + data);
-        console.log('status: ' + status);
-      });
+        responsePromise.error(function(data, status) {
+          console.log('error on updateSummonerFromURL...');
+          console.log('data: ' + data);
+          console.log('status: ' + status);
+        });
 
-    };
+      };
 
-    $scope.getTaskState = function(taskID) {
-      var responsePromise = $http({
-        method: 'POST',
-        url: 'http://127.0.0.1:8001/api/task_state',
-        data: {task_id: taskID},
-        headers: {'content-type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        }
-      });
+      // Match History
 
-      responsePromise.success(function(data, status) {
-        console.log('success on getTaskState...');
-        console.log('data: ' + data);
-        console.log('status: ' + status);
-      })
-    };
+      $scope.games = matchHistoryService.get({region: $routeParams.region, name: $routeParams.name});
+
+      $scope.update = function() {
+        $scope.games = matchHistoryService.get({region: $routeParams.region, name: $routeParams.name});
+      };
 }]);
 
 lolApp.controller('SummonerLookupCtrl', ['$scope', '$timeout', '$location', 'summonerService',
@@ -155,6 +151,10 @@ lolApp.controller('SummonerLookupCtrl', ['$scope', '$timeout', '$location', 'sum
 lolApp.controller('MatchHistoryCtrl', ['$scope', '$routeParams', 'matchHistoryService',
   function($scope, $routeParams, matchHistoryService) {
     $scope.games = matchHistoryService.get({region: $routeParams.region, name: $routeParams.name});
+
+    $scope.update = function() {
+      $scope.games = matchHistoryService.get({region: $routeParams.region, name: $routeParams.name});
+    };
 }]);
 
 lolApp.controller('ItemCtrl', ['$scope',
