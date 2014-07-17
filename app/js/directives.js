fd9f66f399a9e4148d87a230db18d2658e010b41
Edward Chen <kreychek@gmail.com>
2014-07-17 15:12:04 -0400

    Added a directive for gameBox element of the summoner-detail page which will contain each match's data. However, AngularJS is a client to my API, and clients shouldn't have to jump through hoops to use data. So I will be reworking my API to present data that is useable w/o transforms on the Angular side.

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,25 @@
 
 /* Directives */
 
+var lolApp = angular.module('lolApp.directives', []);
 
-angular.module('lolApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+lolApp.directive('appVersion', ['version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+}]);
+
+lolApp.directive('summonerBasicInfo', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/summoner-basic-info.html'
+  };
+});
+
+// Render a gameBox
+lolApp.directive('recentGame', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/recent-game.html'
+  };
+});
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,18 @@
 
 /* Filters */
 
-angular.module('lolApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
+var lolApp = angular.module('lolApp.filters', []);
+
+lolApp.filter('interpolate', ['version', function(version) {
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  };
+}]);
+
+lolApp.filter('gameDuration', function() {
+  return function(sec) {
+    var mm = sec / 60;
+
+    return mm.toFixed(0);
+  };
+});
